d329dab1907314219f010b63dc6ad0ddd5ed56dc
Leonardo Lucena <leorlucena@gmail.com>
2024-06-27 14:26:40 -0300

    Exercicio de membros estaticos e dolar feito

---
+++ Exercicio_Dolar/ConversorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio_Dolar
+{
+    internal class ConversorDeMoeda
+    {
+        public static double cotacao;
+
+        public static double IOF = 6.0;
+        
+        public static double Conversor(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * IOF / 100.0;
+        }
+    }
+}
---
+++ Exercicio_Dolar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+            // uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+            // que a pessoa terá que pagar 6 % de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+            // para ser responsável pelos cálculos.
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = ConversorDeMoeda.Conversor(dolares, cotacao);
+
+            Console.WriteLine($"Valor a ser pago em reais = {total.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
