b1ffd55069bc41bf217fb84ef62d89c399aa6313
Leonardo Lucena <leorlucena@gmail.com>
2024-07-07 10:29:22 -0300

    Acrescentando aula sobre Structs e tipos de valores

---
+++ Structs/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;      //Não precisa instanciar
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+
+        }
+    }
+}
