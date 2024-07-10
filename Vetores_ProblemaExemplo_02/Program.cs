39e6ff5b8bd4ca6a6bb8dac02ae1d464663acc6b
Leonardo Lucena <leorlucena@gmail.com>
2024-07-10 10:36:53 -0300

    Aula sobre vetores com classes finalizada.

---
+++ Vetores_ProblemaExemplo_02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores_ProblemaExemplo_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler um número inteiro N e os dados (nome e
+            //preço) de N Produtos.Armazene os N produtos em um vetor.Em
+            //seguida, mostrar o preço médio dos produtos.
+
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0;i < n;i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine($"AVERAGE PRICE = {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
