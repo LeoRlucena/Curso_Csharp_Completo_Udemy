d03af0abc829fdf6ffb28532fff2fc00ac28a63e
Leonardo Lucena <leorlucena@gmail.com>
2024-07-13 13:49:18 -0300

    Aula sobre o modificador Params

---
+++ Modificador_Params/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Modificador_Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 5);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
