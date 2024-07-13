d3eb1a89a3082142e0fdd25deace9cecd3d053a4
Leonardo Lucena <leorlucena@gmail.com>
2024-07-13 13:58:51 -0300

    Aula sobre os modificadores Ref e Out

---
+++ Modificador_Ref_Out/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Modificador_Ref_Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //Calculator.Triple(ref a);            ref
+            //Console.WriteLine(a);
+
+            int a = 10;
+            int triple;
+            Calculator.Triplo(a, out triple);      // out
+            Console.WriteLine(triple);
+        }
+    }
+}
