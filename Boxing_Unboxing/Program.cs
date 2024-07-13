8dd06ed5c3b5012b7e39111dd03cc10e79466fef
Leonardo Lucena <leorlucena@gmail.com>
2024-07-13 14:09:34 -0300

    Aulas sobre Boxing, Unboxing e foreach realizadas

---
+++ Boxing_Unboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Boxing_Unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+
+            Object obj = x;        //custo de processamento e memoria
+
+            int n = 20;
+
+            Object ob = n;
+
+            int z = (int)ob;
+        }
+    }
+}
