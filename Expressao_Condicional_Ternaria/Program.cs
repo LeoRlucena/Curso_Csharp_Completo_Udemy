a674c4ac722359b7feaa2dd1473b61d44d6e9f55
Leonardo Lucena <leorlucena@gmail.com>
2024-07-18 13:15:56 -0300

    Aulas concluidas

---
+++ Expressao_Condicional_Ternaria/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Expressao_Condicional_Ternaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;   //Utilizando expressao
+
+            // if (preco < 20)
+            // {
+            //     desconto = preco * 0.1;
+            // }
+            // else
+            // {
+            //     desconto = preco * 0.05;
+            // 
+
+
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
---
+++ Palavra_var/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Palavra_var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+            var b = 10f;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(b);
+        }
+    }
+}
