1ed4d7ac30df93c6d59523d4206c854eef149314
Leonardo Lucena <leorlucena@gmail.com>
2024-07-25 08:34:47 -0300

    Exercicio sobre StringBuilder

---
+++ Fixacao_Composicao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fixacao_Composicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
