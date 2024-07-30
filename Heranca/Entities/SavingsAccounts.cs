4f800a3079317dd08b938ff2f1312b31c16b66da
Leonardo Lucena <leorlucena@gmail.com>
2024-07-30 07:30:09 -0300

    Sealling classes and methods

--- Heranca/Entities/SavingsAccounts.cs
+++ Heranca/Entities/SavingsAccounts.cs
@@ -22,7 +22,7 @@ namespace Heranca.Entities
             Balance += Balance * InterestRate;
         }
 
-        public override void Withdraw(double amount)
+        public sealed override void Withdraw(double amount)
         {
             base.Withdraw(amount);
             Balance -= 2.0;
