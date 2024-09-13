868f7070ffa4e20597d78ca0b8d39dd9ba6e5da6
Leonardo Lucena <leorlucena@gmail.com>
2024-09-12 21:38:23 -0300

    Aula sobre FileStream e StreamReader

---
+++ FileStream_StreamReader/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FileStream_StreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\file1.txt";
+            
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();                
+            }
+        }
+    }
+}
