6818f7dcfcd059ea9c53de75e9f6be940362c9e9
Leonardo Lucena <leorlucena@gmail.com>
2024-09-14 10:59:50 -0300

    Bloco using e OpenText

---
+++ Using_Block/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Using_Block
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\file1.txt";
+
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) //OpenText instancia o FileStream e o StreamReader acima dele.
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
