85726192b4ccaa4ae172432111cdb93f45b6d24b
Leonardo Lucena <leorlucena@gmail.com>
2024-09-16 13:16:04 -0300

    secao finalizada

---
+++ Directories/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Directories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\myfolder";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+
+                Directory.CreateDirectory(@"c:\Temp\myfolder\newfolder");
+
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Metadata;
+using System.IO;
+
+namespace StreamWriterAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcepath = @"c:\Temp\file1.txt";
+            string targetpath = @"c:\Temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
