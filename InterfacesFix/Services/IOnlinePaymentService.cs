fc62bcbbe11a3ec3c954e4b3caf84202b098bb92
Leonardo Lucena <leorlucena@gmail.com>
2024-09-19 11:11:59 -0300

    Exercicio resolvido

---
+++ InterfacesFix/Services/IOnlinePaymentService.cs
@@ -0,0 +1,10 @@
+﻿namespace InterfacesFix.Services
+{
+    internal interface IOnlinePaymentService
+    {
+        public double paymentFee(double amount);
+
+        public double Interest(double amount, int months);
+        
+    }
+}
---
+++ InterfacesFix/Services/PaypalService.cs
@@ -0,0 +1,18 @@
+﻿namespace InterfacesFix.Services
+{
+    internal class PaypalService : IOnlinePaymentService
+    {
+        private const double FeePercentage = 0.02;
+        private const double MonthlyInterest = 0.01;
+
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double paymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }        
+    }
+}
