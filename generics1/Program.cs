d4082c5ce80b1f6bc3ed4eda0a4d86b43bf2e313
Leonardo Lucena <leorlucena@gmail.com>
2024-09-20 16:19:38 -0300

    Exercicio resolvido com generics parametrizando com o tipo T

--- generics1/Program.cs
+++ generics1/Program.cs
@@ -4,20 +4,16 @@
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                string x = Console.ReadLine();
+                int x = int.Parse(Console.ReadLine());
                 printService.AddValue(x);
             }
-
-            int a = (int)printService.First();
-            int b = a + 2;
-            Console.WriteLine(b);
 
             printService.Print();
             Console.WriteLine($"First: {printService.First()}");
