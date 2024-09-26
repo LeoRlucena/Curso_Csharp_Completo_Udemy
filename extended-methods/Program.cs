e8dd260023aaf8bbbd8e9c8c957a227f2802851f
Leonardo Lucena <leorlucena@gmail.com>
2024-09-26 16:35:37 -0300

    lesson about extended methods done

---
+++ extended-methods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace extended_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 12, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
