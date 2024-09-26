30baa30b757b09579d0e001ab5f11580b0cabe5c
Leonardo Lucena <leorlucena@gmail.com>
2024-09-26 16:11:06 -0300

    exercise about dictionaries done

---
+++ exercise-dictionaries/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace exercise_dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!(sr.EndOfStream))
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
