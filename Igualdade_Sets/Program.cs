66eb0db01820a7f9b892e815c19dcf32205d230c
Leonardo Lucena <leorlucena@gmail.com>
2024-09-26 10:17:31 -0300

    comparando sets

---
+++ Igualdade_Sets/Program.cs
@@ -0,0 +1,24 @@
+﻿using Igualdade_Sets.Entities;
+
+namespace Igualdade_Sets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> products = new HashSet<Product>();
+            products.Add(new Product("TV", 900.0));
+            products.Add(new Product("Notebook", 1300.0));
+
+            HashSet<Point> points = new HashSet<Point>();
+            points.Add(new Point(3, 4));
+            points.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1300.0);
+            Console.WriteLine(products.Contains(prod));
+
+            Point P = new Point(5, 10);
+            Console.WriteLine(points.Contains(P)); 
+        }
+    }
+}
