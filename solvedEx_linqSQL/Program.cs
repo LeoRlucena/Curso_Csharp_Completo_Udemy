69697ce3406129d9a21d1908062671742d870339
Leonardo Lucena <leorlucena@gmail.com>
2024-10-02 19:55:22 -0300

    solved exercise about linq and lambda sentences

---
+++ solvedEx_linqSQL/Program.cs
@@ -0,0 +1,36 @@
+﻿using solvedEx_linqSQL.Entities;
+
+namespace solvedEx_linqSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = Double.Parse(fields[1]);
+                    list.Add(new Product(name, price));
+                
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2"));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
