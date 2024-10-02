49d188beea99939cdd7bb56927538e23c12c102f
Leonardo Lucena <leorlucena@gmail.com>
2024-10-02 09:28:17 -0300

    introduction to linq

---
+++ linq_introduction/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace linq_introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = { 1, 2, 3 };
+
+            // Define the query expression
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
