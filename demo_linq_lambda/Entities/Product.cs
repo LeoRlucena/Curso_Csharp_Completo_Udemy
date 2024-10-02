cec46befcc5ea4a9ba71cf8f657e3f193e91ea52
Leonardo Lucena <leorlucena@gmail.com>
2024-10-02 10:18:45 -0300

    lambda with linq

---
+++ demo_linq_lambda/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_linq_lambda.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2")
+                + ", "
+                + Category.Tier;
+        }
+    }
+}
